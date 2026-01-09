9a9f8c8c3a8504e7f6a56f0a9097e53231edcf52
rohansingh847986github <rohansingh847986@gmail.com>
2026-01-09 09:20:00 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node js, not browser
+
+// Primitive Data Types
+
+// String, Number, BigInt, Boolean, Null, Undefined, Symbol
+
+let name = 'Rohan'
+let number = 1234
+let loggedIn = true || false
+console.table([name, number, loggedIn])
+
+console.log(typeof(name));
+console.log(typeof "Rohan");
+console.log(typeof 123);
+console.log(typeof null); // Null is Object
+console.log(typeof undefined); // Undefined
+
+
+
+

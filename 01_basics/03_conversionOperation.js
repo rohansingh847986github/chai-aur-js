8df70a2c3e288d8977f43c1b571de23a23253c31
rohansingh847986github <rohansingh847986@gmail.com>
2026-01-09 17:12:27 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,82 @@
+// let score = 123
+// console.log(typeof score);
+
+// let valueInString = String(score)
+
+// console.log('Type of valueInString is - ', typeof valueInString);
+// console.log(valueInString);
+
+
+
+
+// let score1 ='abc'
+// console.log(typeof score1);
+
+// let valueInNumber = Number(score1)
+
+// console.log('Type of valueInNumber is - ',typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let score2 =null
+// console.log(typeof score2);
+// console.log(typeof(score2));
+
+
+// let valueInNumber = Number(score2)
+
+// console.log('Type of valueInNumber is - ',typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let score3 =true
+// console.log(typeof score3);
+// let value = Number(score3)
+// console.log(typeof value);
+// console.log(value);
+
+
+// let score4 = undefined
+// console.log(typeof score4);
+// let value = Number(score4)
+// console.log(typeof value);
+// console.log(value);
+
+// Note:-
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn = "rohan"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// Note:-
+// 1 => true ;false => 0
+// "" => false
+// rohan => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+
+

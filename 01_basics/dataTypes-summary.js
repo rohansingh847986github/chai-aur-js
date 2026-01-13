c6277b71d757777abbe9811c21ace0a0082e73f8
rohansingh847986github <rohansingh847986@gmail.com>
2026-01-13 15:34:00 +0000

    datatype summary

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -14,14 +14,33 @@ const bigNumber = 563472634737653723n
 // Non-Primitive Datatypes - call by reference
 // Array, Object, Function
 
-const array = ['Rohan', 26]
-const obj = {
-    name: 'Rohan',
-    age: 26
+// const array = ['Rohan', 26]
+// const obj = {
+//     name: 'Rohan',
+//     age: 26
+// }
+
+// const myFunction = function(){
+//     console.log('Hello World');
+// }
+// console.log(myFunction);
+
+// ------ Stack Memory & Heap Memory --------
+
+// Primitive Datatypes use Stack Memory - Stack Memory gives you copy of variable and make changes in copy of variable
+let myName = 'Rohan Singh'
+let myNickName = myName
+myNickName = 'X'
+console.log(myName);
+console.log(myNickName);
+
+// Non-Primitive Datatypes use Heap Memory - Heap Memory gives you reference of that variable and direct changes in variable in the memoroy
+let userOne = {
+    email: 'rohan123@gmail.com',
+    upi:'user@bl'
 }
 
-const myFunction = function(){
-    console.log('Hello World');
-}
-console.log(myFunction);
-
+let userTwo = userOne
+userTwo.email = 'rohansingh@gmail.com'
+console.log(userOne);
+console.log(userTwo);

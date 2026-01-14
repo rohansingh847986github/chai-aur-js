e3a8d0488805e92a4b9665a951c1e5bb90d38c83
rohansingh847986github <rohansingh847986@gmail.com>
2026-01-14 14:01:42 +0000

    math and num

---
+++ 01_basics/05_nums_and_math.js
@@ -0,0 +1,55 @@
+const num1 = 100
+// console.log(num1);
+
+const num2 = new Number(100)
+// console.log(num2);
+// console.log(num2.toString().length);
+// console.log(num2.toFixed(2));
+
+const num3 = 23.89
+// console.log(num3.toPrecision(2));
+// console.log(num3.toPrecision(1));
+
+const num4 = 123.8966
+// console.log(num4.toPrecision(2));
+// console.log(num4.toPrecision(3));
+
+const num5 = 1123.8976
+// console.log(num5.toPrecision(2));
+// console.log(num5.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// -------- Maths ---------
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(3,4,2,1));
+// console.log(Math.max(3,6,2,7,84,2))
+// console.log(Math.random());
+// console.log((Math.random()*10));
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min + 1)) + min );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
